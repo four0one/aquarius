9cef954053f5cd05cee57db268d30dc1c013148a
four0one <442350785@qq.com>
2018-12-20 16:26:53 +0800

    加大连接数量

--- src/main/java/com/rpcframework/register/ServiceRegister.java
+++ src/main/java/com/rpcframework/register/ServiceRegister.java
@@ -1,8 +1,12 @@
 package com.rpcframework.register;
 
+import com.rpcframework.monitor.ServiceModel;
+
 /**
  * @author wei.chen1
  * @since 2018/6/6
  */
 public interface ServiceRegister {
+
+	void regist(ServiceModel serviceModel);
 }
